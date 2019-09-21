f5a668c0b50e23bfdd3bdb957294eb19ea4da35f
Nimish Santosh <nimishsantosh107@icloud.com>
2019-09-21 14:47:41 +0530

    server fine

--- base.py
+++ base.py
@@ -14,16 +14,17 @@ payload = identifier.encode('utf8')
 chirp.send(payload, blocking=True)
 
 class Callbacks(CallbackSet):
-    def on_received(self, payload, channel):
-        if payload is not None:
-            identifier = payload.decode('utf-8')
-            print('RECV: ' + identifier)
-            localDICT["id"] = str(uuid4())
-            localDICT["data"] = identifier
-            with open('./data.json', 'w') as fp:
-            	json.dump(localDICT, fp)
-        else:
-            print('DECODE FAILED')
+	def on_received(self, payload, channel):
+		print('hi')
+		if (payload is not None):
+			identifier = payload.decode('utf-8')
+			print('RECV: ' + identifier)
+			localDICT["id"] = str(uuid4())
+			localDICT["data"] = identifier
+			with open('./data.json', 'w') as fp:
+				json.dump(localDICT, fp)
+		else:
+			print('DECODE FAILED')
 
 while True:
 	time.sleep(0.1)
