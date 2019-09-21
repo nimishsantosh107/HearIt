08b9d09f60e8676b8439f79e40ce1aed3d513cb0
Nimish Santosh <nimishsantosh107@icloud.com>
2019-09-21 15:53:02 +0530

    handling quitting

--- base/base.py
+++ base/base.py
@@ -1,17 +1,13 @@
 from chirpsdk import ChirpSDK, CallbackSet
 from uuid import uuid4
 import time
+import threading
 import json
-
-#LOCAL DATA
-localDICT = {}
-
-chirp = ChirpSDK()
-chirp.start(send=True,receive=True)
+import signal
+import sys
 
 class Callbacks(CallbackSet):
 	def on_received(self, payload, channel):
-		print('hi')
 		if (payload is not None):
 			identifier = payload.decode('utf-8')
 			print('RECV: ' + identifier)
@@ -22,14 +18,39 @@ class Callbacks(CallbackSet):
 		else:
 			print('DECODE FAILED')
 
+#RECEIVING LISTENER
 def receive():
-	while True:
+	while(runStatus):
 		time.sleep(0.1)
 		chirp.set_callbacks(Callbacks())
 
+#SENDER
 def send(message):
-	identifier = '$123.456'
+	identifier = message
 	payload = identifier.encode('utf8')
 	chirp.send(payload, blocking=True)
 
-send()
+#HANDLE QUIT
+def signal_handler(signal, frame):
+	print("EXITING")
+	runStatus = False;
+	sys.exit(0)
+
+if __name__ == '__main__':
+	#LOCAL DATA
+	localDICT = {}
+	#CHIRP SETUP
+	chirp = ChirpSDK()
+	chirp.start(send=True,receive=True)
+
+	#RECV DAEMON
+	runStatus = True
+	recvDaemon = threading.Thread(name='RECV DAEMON', target=receive)
+	recvDaemon.setDaemon(True)
+	recvDaemon.start()
+
+	send('$123.456')
+
+	#QUIT PROGRAM
+	signal.signal(signal.SIGINT, signal_handler)
+	recvDaemon.join()
